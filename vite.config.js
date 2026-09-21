6c0265573ae017bebe43106653deff68b608c66c
NewDude-v01 <githubofficialv01@gmail.com>
2026-09-21 14:04:51 +0800

    git issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calculator/', // Replace 'calculator' with your exact GitHub repo name if different
 })
